bc4afd927ab244cda2664880bcf08e519adb59e2
simplyadrian <simplyadrian@gmail.com>
2015-02-13 14:18:44 -0600

    [ISE-340] reverted default attributes to home folder to suppor the idea of generic systems and the override will be done in the role cookbook.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,10 +4,10 @@ default['ipynb-nativex']['linux_user'] = "ipynb"
 # iPython group
 default['ipynb-nativex']['linux_group'] = "ipynb"
 # iPython Notebook user home directory
-default['ipynb-nativex']['home_dir'] = "/srv/ipynb"
+default['ipynb-nativex']['home_dir'] = "/home/ipynb"
 # iPython Notebook working directory
-default['ipynb-nativex']['notebook_dir'] = "/srv/ipynb/notebooks"
+default['ipynb-nativex']['notebook_dir'] = "/home/ipynb/notebooks"
 # iPython Notebook listening address
 default['ipynb-nativex']['ip'] = node['ipaddress']
 # iPython Notebook listening port
-default['ipynb-nativex']['port'] = 80
+default['ipynb-nativex']['port'] = 8888
