944d7ae009ddb5ff93b2dc6d03e12361f029c4d8
simplyadrian <simplyadrian@gmail.com>
2015-02-13 13:17:08 -0600

    fixed install recipe to find correct packages for ipython-notebook and its dependecies

--- recipes/install.rb
+++ recipes/install.rb
@@ -7,20 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'python'
+
 # Install packages
 %w{
-  python-pandas
-  python-numpy
-  python-scipy
-  python-matplotlib
-  python-nose
-  ipython-notebook
+  python-ipython-notebook
+  python33-scipy
 }.each do |pkg|
   package pkg do
     action :upgrade
   end
 end
 
-# iPython needs sympy
-# So use [pip install] instead of package install (0.7.1).
+# iPython needs sympy (0.7.2)
+# So use [pip install] instead of package install (0.6.3).
 python_pip "sympy"
