26d12fff3e7c5c0a90828aa57374b89ec4a025d6
simplyadrian <simplyadrian@gmail.com>
2015-02-13 14:06:35 -0600

    update to default attributes. changing the location of the home directory for the ipynb user to be outside of the nfs mounted /home direcotry

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,9 +4,9 @@ default['ipynb-nativex']['linux_user'] = "ipynb"
 # iPython group
 default['ipynb-nativex']['linux_group'] = "ipynb"
 # iPython Notebook user home directory
-default['ipynb-nativex']['home_dir'] = "/home/ipynb"
+default['ipynb-nativex']['home_dir'] = "/srv/ipynb"
 # iPython Notebook working directory
-default['ipynb-nativex']['notebook_dir'] = "/home/ipynb/notebooks"
+default['ipynb-nativex']['notebook_dir'] = "/srv/ipynb/notebooks"
 # iPython Notebook listening address
 default['ipynb-nativex']['ip'] = node['ipaddress']
 # iPython Notebook listening port
