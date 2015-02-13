6b379bd13fe0e52698715a5827148e734e3f9cd8
Adrian Herrera <simplyadrian@gmail.com>
2015-02-13 11:36:43 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'ipynb-nativex'
 maintainer       'NativeX'
 maintainer_email 'adrian.herrera@nativex.com'
 license          'All rights reserved'
-description      'Installs/Configures ad-nativex'
+description      'Installs/Configures ipynb-nativex'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
